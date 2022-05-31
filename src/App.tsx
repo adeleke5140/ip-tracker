dda863e78ab8d1b40bde8f82de79cf3ffd09aaa8
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-05-31 22:13:26 +0100

    update the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import Layout from "./components/Layout"
+import HeaderTitle from "./components/HeaderTitle"
+import SearchForm from "./components/SearchBar"
+
 function App() {
   return (
     <div className="App">
-      <h1>Ip Tracker</h1>
+      <Layout>
+        <HeaderTitle title="IP Address Tracker" />
+        <SearchForm />
+      </Layout>
     </div>
   )
+}
+
+function Button() {
+  return <button>click me</button>
 }
 
 export default App
